60cfe55b596dfbcbe1730155ebb9def7cc195281
aldomaez <alfredojmartinez@hotmail.com>
2021-07-06 13:54:30 -0400

    Update settings.py

--- django_hello/settings.py
+++ django_hello/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL =  os.path.join(BASE_DIR, '/dealebayfinder/static/')
+STATIC_URL = '/dealebayfinder/static/'
+STATIC_URL =  os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/dealebayfinder/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
