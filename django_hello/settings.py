034b56a685fa64d95875cfaa5861556d4f005ceb
aldomaez <alfredojmartinez@hotmail.com>
2021-07-06 14:15:26 -0400

    Update settings.py

--- django_hello/settings.py
+++ django_hello/settings.py
@@ -120,12 +120,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+"""
 STATIC_URL = '/dealebayfinder/static/'
 STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/dealebayfinder/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+"""
+STATIC_URL = '/static/'
+STATIC_ROOT =  ('static')
 
 LOGIN_REDIRECT_URL = 'dashboard'
 
